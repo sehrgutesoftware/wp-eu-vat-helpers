918e836a820b27f5487e693784b63c2ce3a0aad8
Joseph Paul <mail@jsph.pl>
2017-08-29 13:05:20 +0200

    Add basic test

--- tests/unit/PluginTest.php
+++ tests/unit/PluginTest.php
@@ -2,12 +2,16 @@
 
 namespace SehrGut\WpPricesWithEuVat\Tests\Unit;
 
+use Mpociot\VatCalculator\VatCalculator;
+use SehrGut\WpPricesWithEuVat\Plugin;
 use SehrGut\WpPricesWithEuVat\Tests\TestCase;
 
 class PluginTest extends TestCase
 {
-    public function test_it_asserts_true()
+    public function test_it_constructs_an_instance_of_the_plugin()
     {
-        $this->assertTrue(true);
+        $plugin = new Plugin(new VatCalculator());
+
+        $this->assertTrue($plugin instanceof Plugin);
     }
 }
